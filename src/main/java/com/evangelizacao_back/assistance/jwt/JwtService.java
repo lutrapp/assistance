246fb26ad88d38653a84d312fce1917f78ed5572
lutrapp <lutrapp@gmail.com>
2025-02-13 20:29:01 -0300

    jwt expiretion fixed to 5

--- src/main/java/com/evangelizacao_back/assistance/jwt/JwtService.java
+++ src/main/java/com/evangelizacao_back/assistance/jwt/JwtService.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -28,7 +27,7 @@ public class JwtService {
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + jwtProperties.getExpiration()))
-                . signWith(getSigningKey())
+                .signWith(getSigningKey())
                 .compact();
     }
 
